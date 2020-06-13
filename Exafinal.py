3bc80e766f9e0ec60929faa4ec5f388485da9663
Carlos Marroquin <Carlosema322@gmail.com>
2020-06-13 15:49:19 +0100

    Ventana y titulo

--- Exafinal.py
+++ Exafinal.py
@@ -6,3 +6,11 @@ import math
 import datetime
 class Desk:
     def __init__(self, window,):
+#Ventana del programa
+        ancho1 = 400
+        altura1 = 200
+        self.wind = window
+        self.wind.geometry(str(ancho1)+'x'+str(altura1))
+        #TITULO
+        self.wind.columnconfigure(0, weight=1)
+        self.wind.title('EXAMEN ISC')
