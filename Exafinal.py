c2896341d626398f00ead97a69b69d116844896f
Carlos Marroquin <Carlosema322@gmail.com>
2020-06-13 15:51:26 +0100

    Nombre y apellido del usuario

--- Exafinal.py
+++ Exafinal.py
@@ -17,3 +17,12 @@ class Desk:
         #ENTRADA
         frame = LabelFrame(self.wind, text = 'Bienvenido!!! rellene lo que se le solicita')
         frame.grid(row = 0, column = 2, columnspan = 20, pady = 20)
+       #NOMBRE DEL USUARIO
+        Label(frame, text = 'Nombre: ').grid(row = 1, column = 0)
+        self.p1 = Entry(frame)
+        self.p1.focus()
+        self.p1.grid(row = 1, columnspan = 6)
+        #APELLIDO DEL USUARIO
+        Label(frame, text = 'Apellido.: ').grid(row = 2, column = 0)
+        self.op2 = Entry(frame)
+        self.op2.grid(row = 2, columnspan = 6)
